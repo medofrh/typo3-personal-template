ebebc6ebe260b0217b5faacb36df6597f45703ed
test <medo0o0frh@gmail.com>
2023-11-16 11:54:47 +0100

    Add new files and update dependencies ext12

--- packages/ext12/Configuration/TCA/Overrides/pages.php
+++ packages/ext12/Configuration/TCA/Overrides/pages.php
@@ -2,12 +2,6 @@
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
     'ext12',
-    'Configuration/TSconfig/Page/backend_layout.tsconfig',
+    'Configuration/TSconfig/page.tsconfig',
     'ext12 Backend Layouts'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'ext12',
-    'Configuration/TSconfig/Page/general.tsconfig',
-    'General Page TSConfig'
 );
